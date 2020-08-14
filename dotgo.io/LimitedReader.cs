5098a5c4e97c1a33c37e961a9dbf9868877662e8
ynse <laptop@ynse.nl>
2020-08-14 23:23:45 +0200

    Global methods as extension methods of object class

--- dotgo.io/LimitedReader.cs
+++ dotgo.io/LimitedReader.cs
@@ -24,7 +24,7 @@ namespace dotgo.io
             {
                 return (0, io.EOF );
             }
-            if (dotgo.len(p) > l.N)
+            if (this.len(p) > l.N)
             {
                 //p = new slice<byte>(p, 0, (int)N);
             }
--- dotgo/dotgo.cs
+++ dotgo/dotgo.cs
@@ -5,12 +5,12 @@ namespace dotgo
 {
     public static class dotgo
     {
-        public static T[] append<T>(T[] slice, params T[] elems)
+        public static T[] append<T>(this object o, T[] slice, params T[] elems)
         {
             return slice.Concat(elems).ToArray();
         }
 
-        public static int cap(object v)
+        public static int cap(this object o, object v)
         {
             int cap = 0;
             Type target = v.GetType();
@@ -22,41 +22,41 @@ namespace dotgo
             return cap;
         }
 
-        public static void close<T>(chan<T> c)
+        public static void close<T>(this object o, chan<T> c)
         {
             //TODO: Implement
         }
 
-        public static complex64 Complex(float r, float i) {
+        public static complex64 Complex(this object o, float r, float i) {
             return new complex64(r, i);
         }
 
-        public static complex128 Complex(double r, double i) {
+        public static complex128 Complex(this object o, double r, double i) {
             return new complex128(r, i);
         }
 
-        public static int copy<T>(T[] dst, T[] src)
+        public static int copy<T>(this object o, T[] dst, T[] src)
         {
             Array.Copy(src, dst, src.Length);
             return src.Length;
         }
 
-        public static void delete<T, U>(map<T, U> m, T key)
+        public static void delete<T, U>(this object o, map<T, U> m, T key)
         {
             m.delete(key);
         }
 
-        public static float imag(complex64 c)
+        public static float imag(this object o, complex64 c)
         {
             return c.i;
         }
 
-        public static double imag(complex128 c)
+        public static double imag(this object o, complex128 c)
         {
             return c.i;
         }
 
-        public static int len(object v)
+        public static int len(this object o, object v)
         {
             int len = 0;
             Type target = v.GetType();
@@ -68,24 +68,24 @@ namespace dotgo
             return len;
         }
 
-        public static T make<T>(params int[] size)
+        public static T make<T>(this object o, params int[] size)
         {
             //TODO: Implement
             return default(T);
         }
 
-        public static object make(Type t, params int[] size)
+        public static object make(this object o, Type t, params int[] size)
         {
             //TODO: Implement
             return null;
         }
 
-        public static void panic()
+        public static void panic(this object o)
         {
             //TODO: Implement
         }
 
-        public static void print(params object[] args)
+        public static void print(this object o, params object[] args)
         {
             foreach(var item in args)
             {
@@ -93,23 +93,23 @@ namespace dotgo
             }
         }
 
-        public static void println(params object[] args)
+        public static void println(this object o, params object[] args)
         {
             print(args);
             Console.WriteLine();
         }
 
-        public static float real(complex64 c)
+        public static float real(this object o, complex64 c)
         {
             return c.r;
         }
 
-        public static double real(complex128 c)
+        public static double real(this object o, complex128 c)
         {
             return c.r;
         }
 
-        public static void recover()
+        public static void recover(this object o)
         {
             //TODO: Implement
         }
